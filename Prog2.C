77c576d056fd470ff3ab88feb51bcff0ccc6ad13
Sieh <1960681@linux102.mcs.sdsmt.edu>
2015-03-05 11:28:56 -0700

    Added node structure

--- Prog2.C
+++ Prog2.C
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+struct avl_tree_node
+{
+	int key_value; //tree node value
+	int left_child; //index of left child
+	int right_child; //index of right child
+	int height; //height from here down (0 if leaf)
+	int parent; //index of parent (C_NULL if root)
+	int file_loc; //index of this node
+}
+const int C_NULL = -1;
+const int C_NODE_SIZE = sizeof(avl_tree_node);
+
 int main( int argv, char argc[])
 {
 
